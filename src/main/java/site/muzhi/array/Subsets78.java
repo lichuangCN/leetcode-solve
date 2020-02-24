b2833f5e974cf68329220f94abf98d3ef0ada95b
lichuangCN <1503031032@qq.com>
2020-02-24 17:11:28 +0800

    2020/02/24

---
+++ src/main/java/site/muzhi/array/Subsets78.java
@@ -0,0 +1,16 @@
+package site.muzhi.array;
+
+import java.util.List;
+
+/**
+ * @author: LiChuang
+ * @date: 2020/02/24
+ * @description: 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 说明：解集不能包含重复的子集
+ */
+public class Subsets78 {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        return null;
+    }
+}
