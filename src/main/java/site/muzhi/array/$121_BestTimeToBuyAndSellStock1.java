20d2960f951af8d0b465606829ba4a1cc1a896b9
lichuangCN <1503031032@qq.com>
2020-03-09 11:29:46 +0800

    2020/03/09

---
+++ src/main/java/site/muzhi/array/$121_BestTimeToBuyAndSellStock1.java
@@ -0,0 +1,60 @@
+package site.muzhi.array;
+
+/**
+ * @author: lichuang
+ * @date: 2020/03/09
+ * @description: 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），
+ * 设计一个算法来计算你所能获取的最大利润。
+ * 注意你不能在买入股票前卖出股票。
+ * <p>
+ * 思想：需要找出股票价格之间最大的差值，
+ * 并且最小值[i]和最大值prices[j]之中,i < j
+ */
+
+public class $121_BestTimeToBuyAndSellStock1 {
+
+    /**
+     * 假设第i天买入股票，在第i天后的日子内将股票卖出，计算买入卖出的利润值
+     * 时间复杂度O(n)
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit1(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            // 计算第i天之后卖出的最高利润
+            for (int j = i + 1; j < prices.length; j++) {
+                if ((prices[j] - prices[i]) > profit) {
+                    profit = prices[j] - prices[i];
+                }
+            }
+        }
+        return profit;
+    }
+
+    /**
+     * 寻找当前股票最低价格，如果当前价格高于之前找到的最低价格
+     * 则计算当前股票卖出的利润
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            // 如果当前价格低于之前的最小值，则重新设定最小值
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                // 如果当前价格大于最小值，计算当前卖出的利润是否大于之前计算的利润
+                if (prices[i] - minPrice > maxProfit) {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ src/main/java/site/muzhi/array/$746_MinCostClimbingStairs.java
@@ -0,0 +1,22 @@
+package site.muzhi.array;
+
+/**
+ * @author: lichuang
+ * @date: 2020/03/09
+ * @description: 数组的每个索引做为一个阶梯，第 i个阶梯对应着一个非负数的体力花费值 cost[i](索引从0开始)。*
+ * 每当你爬上一个阶梯你都要花费对应的体力花费值，然后你可以选择继续爬一个阶梯或者爬两个阶梯。*
+ * 您需要找到达到楼层顶部的最低花费。在开始时，你可以选择从索引为 0 或 1 的元素作为初始阶梯。
+ */
+
+public class $746_MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        //
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+}
