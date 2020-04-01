21e222951554568ba39009bb1d91766fc8331cd2
lichuangCN <1503031032@qq.com>
2020-04-01 21:57:45 +0800

    2020/04/01

---
+++ src/main/java/site/muzhi/string/$1111_MaxDepthAfterSplit.java
@@ -0,0 +1,29 @@
+package site.muzhi.string;
+
+/**
+ * @author: lichuang
+ * @date: 2020/04/01
+ * @description:
+ */
+
+public class $1111_MaxDepthAfterSplit {
+
+    public int[] maxDepthAfterSplit(String seq) {
+        int[] result = new int[seq.length()];
+
+        int depth = 0, index = 0;
+        for (char c : seq.toCharArray()) {
+            if (c == '(') {
+                // 入栈，栈深度+1
+                depth++;
+                // 奇数or偶数
+                result[index++] = depth & 1;
+            } else {
+                result[index++] = depth & 1;
+                // 出栈，栈深度-1
+                depth--;
+            }
+        }
+        return result;
+    }
+}
