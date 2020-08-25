c16a8d7cbb0f75e487e3a18e93ae8dfba27ec9b0
lichuangCN <1503031032@qq.com>
2020-08-25 15:46:27 +0800

    add 2020-08-25

---
+++ src/main/java/site/muzhi/tree/$107_LevelOrderBottom.java
@@ -0,0 +1,69 @@
+package site.muzhi.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author lichuang
+ * @date 2020/08/25
+ * @description 二叉树的层次遍历 II
+ * <p>
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。
+ * （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ */
+public class $107_LevelOrderBottom {
+
+    /**
+     * Definition for a binary tree node.
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>(0);
+        }
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            // 当前层节点值列表
+            ArrayList<Integer> levelNode = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                levelNode.add(node.val);
+                TreeNode left = node.left;
+                if (left != null) {
+                    queue.add(left);
+                }
+                TreeNode right = node.right;
+                if (right != null) {
+                    queue.add(right);
+                }
+            }
+            res.addFirst(levelNode);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/site/muzhi/tree/$108_SortedArrayToBST.java
@@ -0,0 +1,50 @@
+package site.muzhi.tree;
+
+/**
+ * @author lichuang
+ * @date 2020/08/25
+ * @description 将有序数组转换为二叉搜索树
+ * <p>
+ * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1
+ */
+public class $108_SortedArrayToBST {
+
+    /**
+     * Definition for a binary tree node.
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 中序遍历逆过程
+     *
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        TreeNode root = build(nums, 0, nums.length - 1);
+        return root;
+    }
+
+    public TreeNode build(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = build(nums, left, mid - 1);
+        root.right = build(nums, mid + 1, right);
+        return root;
+    }
+}
