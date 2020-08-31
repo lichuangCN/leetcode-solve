9acd12a46701e24195aaee3f45668897531b50a2
lichuangCN <1503031032@qq.com>
2020-08-31 15:52:30 +0800

    add 2020-08-31

---
+++ src/main/java/site/muzhi/tree/$173_BSTIterator.java
@@ -0,0 +1,69 @@
+package site.muzhi.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author lichuang
+ * @date 2020/08/31
+ * @description 二叉搜索树迭代器
+ */
+public class $173_BSTIterator {
+
+    /**
+     * Definition for a binary tree node.
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class BSTIterator {
+
+        private LinkedList<TreeNode> stack = new LinkedList<>();
+
+        public BSTIterator(TreeNode root) {
+            TreeNode node = root;
+            // 初始化
+            if (node != null) {
+                stack.addFirst(node);
+                while (node.left != null) {
+                    stack.addFirst(node.left);
+                    node = node.left;
+                }
+            }
+        }
+
+        /**
+         * @return the next smallest number
+         */
+        public int next() {
+            if (hasNext()) {
+                TreeNode node = stack.pollFirst();
+                int val = node.val;
+                if (node.right != null) {
+                    stack.addFirst(node.right);
+                    node = node.right;
+                    while (node.left != null) {
+                        stack.addFirst(node.left);
+                        node = node.left;
+                    }
+                }
+                return val;
+            }
+            return 0;
+        }
+
+        /**
+         * @return whether we have a next smallest number
+         */
+        public boolean hasNext() {
+            return stack.size() == 0 ? false : true;
+        }
+    }
+}
