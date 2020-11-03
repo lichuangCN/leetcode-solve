318eb56b7f16d7448ff90e2ab0903ef056ff91a3
lichuang <lichuang@jszmnet.com>
2020-11-03 12:56:49 +0800

    add 2020-11-3

---
+++ src/main/java/site/muzhi/array/$941_ValidMountainArray.java
@@ -0,0 +1,26 @@
+package site.muzhi.array;
+
+/**
+ * @author lichuang
+ * @date 2020/11/03
+ * @description 有效的山脉数组
+ */
+public class $941_ValidMountainArray {
+
+    public boolean validMountainArray(int[] A) {
+        if (A == null || A.length < 3) {
+            return false;
+        }
+        int left = 0, right = A.length - 1;
+        // 从左侧寻找山峰
+        while (left + 1 <= A.length - 1 && A[left + 1] > A[left]) {
+            left++;
+        }
+        // 从右侧寻找山峰
+        while (right - 1 >= 0 && A[right - 1] > A[right]) {
+            right--;
+        }
+        // 是否是同一个山峰
+        return (right == left) && (right > 0) && (left < A.length - 1);
+    }
+}
---
+++ src/main/java/site/muzhi/offer/$9_CQueue.java
@@ -0,0 +1,41 @@
+package site.muzhi.offer;
+
+import java.util.LinkedList;
+
+/**
+ * @author lichuang
+ * @date 2020/11/03
+ * @description 用两个栈实现队列
+ */
+public class $9_CQueue {
+    class CQueue {
+        private LinkedList<Integer> inStack;
+        private LinkedList<Integer> outStack;
+
+        public CQueue() {
+            inStack = new LinkedList<>();
+            outStack = new LinkedList<>();
+        }
+
+        public void appendTail(int value) {
+            inStack.addLast(value);
+        }
+
+        public int deleteHead() {
+            if (outStack.size() == 0) {
+                // 栈内无元素
+                int inSize = inStack.size();
+                if (inSize == 0) {
+                    return -1;
+                }
+                // 将进栈中的元素移动到出栈
+                while (inSize > 0) {
+                    outStack.addLast(inStack.pollLast());
+                    inSize--;
+                }
+                return outStack.pollLast();
+            }
+            return outStack.pollLast();
+        }
+    }
+}
