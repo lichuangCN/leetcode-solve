9c36f05b7802357757a88aeae07089bb060852d6
lichuang <lichuang@jszmnet.com>
2020-11-16 13:09:31 +0800

    add/update 2020-11-16

---
+++ src/main/java/site/muzhi/array/$406_ReconstructQueue.java
@@ -0,0 +1,36 @@
+package site.muzhi.array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+/**
+ * @author lichuang
+ * @date 2020/11/16
+ * @description 根据身高重建队列
+ */
+public class $406_ReconstructQueue {
+    public int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length == 0 || people[0].length == 0) {
+            return new int[0][0];
+        }
+        // 1.先对身高进行降序排序
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] x, int[] y) {
+                if (x[0] == y[0]) {
+                    return x[1] - y[1];
+                } else {
+                    return y[0] - x[0];
+                }
+            }
+        });
+
+        LinkedList<int[]> list = new LinkedList<>();
+        // 2.根据身高和前面人数找自己位置
+        for (int[] person : people) {
+            list.add(person[1], person);
+        }
+        return list.toArray(new int[people.length][2]);
+    }
+}
---
+++ src/main/java/site/muzhi/list/$328_OddEvenList.java
@@ -0,0 +1,52 @@
+package site.muzhi.list;
+
+/**
+ * @author lichuang
+ * @date 2020/11/16
+ * @description 奇偶链表
+ * <p>
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。
+ * 请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ * 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ */
+public class $328_OddEvenList {
+    /**
+     * Definition for singly-linked list.
+     */
+    class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode evenHead = new ListNode(0);
+        ListNode evenTail = evenHead;
+        ListNode oddHead = new ListNode(0);
+        ListNode oddTail = oddHead;
+        ListNode ptr = head;
+        boolean isEven = true;
+        while (ptr != null) {
+            if (isEven) {
+                evenTail.next = ptr;
+                evenTail = ptr;
+                isEven = false;
+            } else {
+                oddTail.next = ptr;
+                oddTail = ptr;
+                isEven = true;
+            }
+            ptr = ptr.next;
+        }
+        oddTail.next = null;
+        evenTail.next = oddHead.next;
+        return evenHead.next;
+    }
+}
