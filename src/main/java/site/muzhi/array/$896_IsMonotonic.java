5d07a76e9b93b24fbb7ffac33c8365a34f8d0419
lichuangCN <lichuangcn@foxmail.com>
2021-02-28 16:04:39 +0800

    2020-02-28

---
+++ src/main/java/site/muzhi/array/$896_IsMonotonic.java
@@ -0,0 +1,26 @@
+package site.muzhi.array;
+
+/**
+ * @author lichuang
+ * @date 2021/02/28
+ * @description 单调数列
+ */
+public class $896_IsMonotonic {
+
+    public boolean isMonotonic(int[] A) {
+        boolean inc = false;
+        boolean dec = false;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > A[i - 1]) {
+                inc = true;
+            }
+            if (A[i] < A[i - 1]) {
+                dec = true;
+            }
+            if (inc && dec) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/site/muzhi/interview/$05_01_InsertBits.java
@@ -0,0 +1,21 @@
+package site.muzhi.interview;
+
+/**
+ * @author lichuang
+ * @date 2021/02/28
+ * @description
+ */
+public class $05_01_InsertBits {
+
+    public int insertBits(int N, int M, int i, int j) {
+        // 1.指定位置置0
+        int mask = ((1 << (j - i + 1)) - 1) << i;
+        mask = ~mask;
+        N &= mask;
+        // 2.处理M
+        M = M << i;
+        // 3.替换
+        N = N | M;
+        return N;
+    }
+}
