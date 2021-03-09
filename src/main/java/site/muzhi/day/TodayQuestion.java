181bb9dc28c93ae2e5d0cd6440814a1a00cac0aa
lichuangCN <lichuangcn@foxmail.com>
2021-03-09 12:47:36 +0800

    2021-03-09

--- src/main/java/site/muzhi/day/TodayQuestion.java
+++ src/main/java/site/muzhi/day/TodayQuestion.java
@@ -1,5 +1,6 @@
 package site.muzhi.day;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -8,58 +9,19 @@ import java.util.Stack;
  * @description 出现已经做过的题目时，在此类中完成此题的代码的编写
  */
 public class TodayQuestion {
-    class MyQueue {
 
-        Stack<Integer> inStack;
-        Stack<Integer> outStack;
-
-        /**
-         * Initialize your data structure here.
-         */
-        public MyQueue() {
-            inStack = new Stack<>();
-            outStack = new Stack<>();
-        }
-
-        /**
-         * Push element x to the back of queue.
-         */
-        public void push(int x) {
-            inStack.push(x);
-        }
-
-        /**
-         * Removes the element from in front of queue and returns that element.
-         */
-        public int pop() {
-            if (!outStack.isEmpty()) {
-                return outStack.pop();
+    public int[] nextGreaterElements(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < len * 2; i++) {
+            while (!stack.isEmpty() && nums[i % len] > nums[stack.peek()]) {
+                res[stack.pop()] = nums[i % len];
             }
-            while (!inStack.isEmpty()) {
-                outStack.push(inStack.pop());
-            }
-            return outStack.pop();
+            stack.push(i % len);
         }
-
-        /**
-         * Get the front element.
-         */
-        public int peek() {
-            if (!outStack.isEmpty()) {
-                return outStack.peek();
-            }
-            while (!inStack.isEmpty()) {
-                outStack.push(inStack.pop());
-            }
-            return outStack.peek();
-        }
-
-        /**
-         * Returns whether the queue is empty.
-         */
-        public boolean empty() {
-            return inStack.isEmpty() && outStack.isEmpty();
-        }
+        return res;
     }
 
 }
