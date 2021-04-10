475f34e60915be78baa09ced51dfc0ae2b68e623
lichuangCN <lichuangcn@foxmail.com>
2021-04-10 18:12:40 +0800

    2021-04-10

---
+++ src/main/java/site/muzhi/more/$263_IsUgly.java
@@ -0,0 +1,27 @@
+package site.muzhi.more;
+
+/**
+ * @author lichuang
+ * @date 2021/04/10
+ * @description 263. 丑数
+ * <p>
+ * https://leetcode-cn.com/problems/ugly-number/
+ */
+public class $263_IsUgly {
+
+    public boolean isUgly(int n) {
+        if (n <= 1) {
+            return n == 1 ? true : false;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+        return n == 1;
+    }
+}
