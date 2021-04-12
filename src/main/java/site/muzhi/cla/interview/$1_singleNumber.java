9a811657f1229624a3e1de681a3398a4b59e2762
lichuangCN <lichuangcn@foxmail.com>
2021-04-12 21:51:18 +0800

    2020-04-12

---
+++ src/main/java/site/muzhi/cla/interview/$1_singleNumber.java
@@ -0,0 +1,19 @@
+package site.muzhi.cla.interview;
+
+/**
+ * @author lichuang
+ * @date 2021/04/12
+ * @description 只出现一次的数字
+ * <p>
+ * https://leetcode-cn.com/leetbook/read/top-interview-questions/xm0u83/
+ */
+public class $1_singleNumber {
+
+    public int singleNumber(int[] nums) {
+        int r = 0;
+        for (int num : nums) {
+            r = (r ^ num);
+        }
+        return r;
+    }
+}
