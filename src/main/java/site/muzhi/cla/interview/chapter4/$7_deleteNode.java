05c3a35d29c604970275bd310798cf7079bd631d
lichuangCN <lichuangcn@foxmail.com>
2021-04-26 20:51:59 +0800

    2020-04-26

---
+++ src/main/java/site/muzhi/cla/interview/chapter4/$7_deleteNode.java
@@ -0,0 +1,37 @@
+package site.muzhi.cla.interview.chapter4;
+
+/**
+ * @author lichuang
+ * @date 2021/04/26
+ * @description 删除链表中的节点
+ * <p>
+ * https://leetcode-cn.com/leetbook/read/top-interview-questions/xadve1/
+ */
+public class $7_deleteNode {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int x) {
+            val = x;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        if (node == null) {
+            return;
+        }
+        // 我变成你，再把你干掉，我就干掉了我自己
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
