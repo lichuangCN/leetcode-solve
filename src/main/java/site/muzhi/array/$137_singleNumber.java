6bc8d842c5ee13d8ea5e71924e446dca6598ba41
lichuangCN <lichuangcn@foxmail.com>
2021-04-30 09:18:49 +0800

    2021-04-30

---
+++ src/main/java/site/muzhi/array/$137_singleNumber.java
@@ -0,0 +1,33 @@
+package site.muzhi.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lichuang
+ * @date 2021/04/30
+ * @description 只出现一次的数字 II
+ * <p>
+ * https://leetcode-cn.com/problems/single-number-ii/
+ */
+public class $137_singleNumber {
+
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            Integer val = map.getOrDefault(num, 0);
+            val++;
+            if (val == 3) {
+                map.remove(num);
+            } else {
+                map.put(num, val);
+            }
+        }
+
+        int res = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            res = entry.getKey();
+        }
+        return res;
+    }
+}
