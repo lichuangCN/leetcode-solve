b2d4c1a084a6cd609cc3816ad2139411a8c28eae
lichuangCN <lichuangcn@foxmail.com>
2021-05-14 14:41:10 +0800

    2021-05-14

---
+++ src/main/java/site/muzhi/cla/primaryalgorithm/chapter2/$3_firstUniqChar.java
@@ -0,0 +1,31 @@
+package site.muzhi.cla.primaryalgorithm.chapter2;
+
+import java.util.HashMap;
+
+/**
+ * @author lichuang
+ * @date 2021/05/14
+ * @description 字符串中的第一个唯一字符
+ * <p>
+ * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnx13t/
+ */
+public class $3_firstUniqChar {
+
+    public int firstUniqChar(String s) {
+        if (s == null || "".equals(s)) {
+            return -1;
+        }
+        int[] count = new int[26];
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i] - 'a']++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (count[arr[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
