a5650c55304785d76471eab0324a237e06120d33
lichuangCN <lichuangcn@foxmail.com>
2021-05-20 17:01:00 +0800

    2021-05-20

---
+++ src/main/java/site/muzhi/cla/primaryalgorithm/chapter4/$3_isSymmetric.java
@@ -0,0 +1,41 @@
+package site.muzhi.cla.primaryalgorithm.chapter4;
+
+/**
+ * @author lichuang
+ * @date 2021/05/20
+ * @description 对称二叉树
+ * <p>
+ * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn7ihv/
+ */
+public class $3_isSymmetric {
+
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+
+}
---
+++ src/main/java/site/muzhi/cla/primaryalgorithm/chapter4/$4_levelOrder.java
@@ -0,0 +1,48 @@
+package site.muzhi.cla.primaryalgorithm.chapter4;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author lichuang
+ * @date 2021/05/20
+ * @description
+ */
+public class $4_levelOrder {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.removeFirst();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.addLast(node.left);
+                }
+                if (node.right != null) {
+                    queue.addLast(node.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
