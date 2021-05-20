acbf2364db6a085cc8fe994db975e1e199f0b7b0
lichuangCN <lichuangcn@foxmail.com>
2021-05-20 13:15:06 +0800

    2021-05-20

---
+++ src/main/java/site/muzhi/cla/primaryalgorithm/chapter3/$1_deleteNode.java
@@ -0,0 +1,25 @@
+package site.muzhi.cla.primaryalgorithm.chapter3;
+
+/**
+ * @author lichuang
+ * @date 2021/05/19
+ * @description 删除链表中的节点
+ * <p>
+ * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnarn7/
+ */
+public class $1_deleteNode {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
