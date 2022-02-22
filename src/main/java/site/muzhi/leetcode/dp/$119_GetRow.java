21ff8e5b7407768db397b009d97ff5a288d08243
lichuangCN <lichuangcn@foxmail.com>
2022-02-22 23:27:35 +0800

    2022-02-22

---
+++ src/main/java/site/muzhi/leetcode/dp/$119_GetRow.java
@@ -0,0 +1,43 @@
+package site.muzhi.leetcode.dp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author lichuang
+ * @date 2022/02/22
+ * @description 119.杨辉三角 II
+ * <p>
+ * https://leetcode-cn.com/problems/pascals-triangle-ii/
+ */
+public class $119_GetRow {
+
+    /**
+     * 本题可从新的一行的末尾开始向前填充
+     * <p>
+     * dp状态转移方程：
+     * i:行数，j行内元素位置
+     * 初始值 row(0,0)=1;
+     * row(i+1,j) = row(i,j)+row(i,j-1)
+     */
+    public List<Integer> getRow(int rowIndex) {
+        if (rowIndex <= 0) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        // i:行循环，从第2行开始
+        for (int i = 1; i <= rowIndex; i++) {
+            // 为新的一行提前添加一个值
+            row.add(0);
+            // 行内循序填充，从第二行开始
+            for (int j = i; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+        }
+
+        return row;
+    }
+}
---
+++ src/main/java/site/muzhi/leetcode/offerII/$003_countBits.java
@@ -0,0 +1,25 @@
+package site.muzhi.leetcode.offerII;
+
+/**
+ * @author lichuang
+ * @date 2022/02/22
+ * @description 剑指 Offer II 003. 前 n 个数字二进制中 1 的个数
+ * <p>
+ * https://leetcode-cn.com/problems/w3tCBm/
+ */
+public class $003_countBits {
+
+    /**
+     * i:偶数 ans[i] = ans[i/2]
+     * i:奇数 ans[i] = ans[i/2] + 1
+     * <p>
+     * 状态转移方程：ans[i] = ans[i/2] + (i&1)
+     */
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+}
