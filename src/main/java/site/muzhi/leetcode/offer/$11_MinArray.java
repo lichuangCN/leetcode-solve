6d71fc826b24c333de8283a5925fda87c7cfe910
lichuangCN <lichuangcn@foxmail.com>
2022-03-03 23:15:58 +0800

    2022-03-03

---
+++ src/main/java/site/muzhi/leetcode/offer/$11_MinArray.java
@@ -0,0 +1,39 @@
+package site.muzhi.leetcode.offer;
+
+/**
+ * @author lichuang
+ * @date 2022/03/03
+ * @description 剑指 Offer 11. 旋转数组的最小数字
+ * <p>
+ * https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/
+ */
+public class $11_MinArray {
+
+    public int minArray(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return -1;
+        }
+        int left = 0, right = numbers.length - 1;
+
+        // 左右两头数值相同
+        while (0 < right && numbers[0] == numbers[right]) {
+            right--;
+        }
+
+        // 排除右侧相同的数值，递增序列，则队首是最小值
+        if (numbers[left] <= numbers[right]) {
+            return numbers[left];
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // mid逐步逼近目标值
+            if (numbers[0] <= numbers[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return numbers[left];
+    }
+}
---
+++ src/main/java/site/muzhi/leetcode/offer/$4_FindNumberIn2DArray.java
@@ -0,0 +1,32 @@
+package site.muzhi.leetcode.offer;
+
+/**
+ * @author lichuang
+ * @date 2022/03/03
+ * @description 剑指 Offer 04. 二维数组中的查找
+ * <p>
+ * https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+ */
+public class $4_FindNumberIn2DArray {
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = matrix.length, col = matrix[0].length;
+
+        // 从右上向左下遍历查询
+        int x = 0, y = col - 1;
+        while (x < row && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] > target) {
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return false;
+    }
+}
