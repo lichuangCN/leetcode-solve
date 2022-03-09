0ec38da966edcc36b4790c1ce737f7e5929a0095
lichuangCN <lichuangcn@foxmail.com>
2022-03-09 23:03:13 +0800

    2022-03-09

--- src/main/java/site/muzhi/leetcode/offer/$18_DeleteNode.java
+++ src/main/java/site/muzhi/leetcode/offer/$18_DeleteNode.java
@@ -3,10 +3,12 @@ package site.muzhi.leetcode.offer;
 /**
  * @author lichuang
  * @date 2020/10/10
- * @description 删除链表的节点
+ * @description 剑指 Offer 18. 删除链表的节点
  * <p>
  * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
  * 返回删除后的链表的头节点
+ * <p>
+ * https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
  */
 public class $18_DeleteNode {
     class ListNode {
@@ -19,22 +21,20 @@ public class $18_DeleteNode {
     }
 
     public ListNode deleteNode(ListNode head, int val) {
-        if (head == null) {
+        if (head == null ) {
             return null;
         }
         if (head.val == val) {
             return head.next;
         }
-        ListNode curr = head, pre = null;
+        ListNode pre = null,curr = head;
         while (curr != null) {
-
-            if (curr.val != val) {
-                pre = curr;
-                curr = curr.next;
-            } else {
+            if (curr.val == val) {
                 pre.next = curr.next;
                 break;
             }
+            pre = curr;
+            curr = curr.next;
         }
         return head;
     }
--- src/main/java/site/muzhi/leetcode/offer/$42_MaxSubArray.java
+++ src/main/java/site/muzhi/leetcode/offer/$42_MaxSubArray.java
@@ -3,12 +3,15 @@ package site.muzhi.leetcode.offer;
 /**
  * @author lichuang
  * @date 2020/10/15
- * @description 连续子数组的最大和
+ * @description 剑指 Offer 42. 连续子数组的最大和
  * <p>
  * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
  * 要求时间复杂度为O(n)。
+ * <p>
+ * https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
  */
 public class $42_MaxSubArray {
+
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
