e477a0e799d70ee56bf581fa11df6642a00a3506
lichuangCN <lichuangcn@foxmail.com>
2022-03-13 22:29:50 +0800

    2022-03-13

--- src/main/java/site/muzhi/leetcode/offer/$55_1_MaxDepth.java
+++ src/main/java/site/muzhi/leetcode/offer/$55_1_MaxDepth.java
@@ -3,11 +3,13 @@ package site.muzhi.leetcode.offer;
 /**
  * @author lichuang
  * @date 2020/09/26
- * @description 二叉树深度
+ * @description 剑指 Offer 55 - I. 二叉树的深度
  * <p>
  * 输入一棵二叉树的根节点，
  * 求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，
  * 最长路径的长度为树的深度。
+ * <p>
+ * https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
  */
 public class $55_1_MaxDepth {
 
