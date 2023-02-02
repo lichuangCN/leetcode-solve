6fd5933f1b6226173ec0cf66cba5a658a97af64f
lichuang <lichuang@staff.sina.com>
2023-02-02 20:36:56 +0800

    2023-02-02

--- src/main/java/site/muzhi/leetcode/hot100/$33_search.java
+++ src/main/java/site/muzhi/leetcode/hot100/$33_search.java
@@ -5,4 +5,32 @@ package site.muzhi.leetcode.hot100;
  * @date 2023/02/02
  */
 public class $33_search {
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
 }
