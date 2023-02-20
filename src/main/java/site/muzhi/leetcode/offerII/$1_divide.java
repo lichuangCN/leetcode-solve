8841b8a02d569ca23fb332fa1481eb6b00e10fc2
lichuang <lichuang@staff.sina.com>
2023-02-20 20:25:07 +0800

    2023-02-20

--- src/main/java/site/muzhi/leetcode/offerII/$1_divide.java
+++ src/main/java/site/muzhi/leetcode/offerII/$1_divide.java
@@ -13,15 +13,21 @@ public class $1_divide {
             return Integer.MAX_VALUE;
         }
 
-        int sign = (a > 0) ^ (b > 0) ? -1 : -1;
+        int sign = (a > 0) ^ (b > 0) ? -1 : 1;
         // 统一转为负值处理
-        int x = (a > 0) ? -a : a;
-        int y = (b > 0) ? -b : b;
-        int result = 0;
-        while (x <= y) {
-            x -= y;
-            result++;
+        if (a > 0) {
+            a = -a;
         }
-        return sign == 1 ? result : -result;
+        if (b > 0) {
+            b = -b;
+        }
+
+        int ans = 0;
+        while (a <= b) {
+            a -= b;
+            ans++;
+        }
+
+        return sign == 1 ? ans : -ans;
     }
 }
---
+++ src/main/java/site/muzhi/leetcode/offerII/$6_twoSum.java
@@ -0,0 +1,26 @@
+package site.muzhi.leetcode.offerII;
+
+/**
+ * @author lichuang
+ * @date 2023/02/20
+ */
+public class $6_twoSum {
+
+    public int[] towSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left, right};
+            }
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+
+        }
+        return ans;
+    }
+}
