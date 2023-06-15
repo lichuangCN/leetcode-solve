f098c1fd388abfed8998544167dfcc46e84989cf
lichuang <lichuang@staff.sina.com>
2023-06-15 20:30:24 +0800

    2023-06-14

---
+++ src/main/java/site/muzhi/leetcode/offerII/$77_sortList.java
@@ -0,0 +1,71 @@
+package site.muzhi.leetcode.offerII;
+
+/**
+ * @author lichuang
+ * @date 2023/06/15
+ * 77.链表排序（归并排序）
+ */
+public class $77_sortList {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head, fast = head, pre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // 切分两个链表
+        pre.next = null;
+        // 归并排序
+        return merge(sortList(head), sortList(slow));
+    }
+
+    /**
+     * 合并有序链表
+     */
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode sentry = new ListNode(), tail = sentry;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if (l1 != null) {
+            tail.next = l1;
+        }
+        if (l2 != null) {
+            tail.next = l2;
+        }
+        return sentry.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
