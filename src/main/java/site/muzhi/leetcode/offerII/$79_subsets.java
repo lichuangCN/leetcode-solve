ac16fd83b4eba0ea210284ba032ea5cd3d4ad206
lichuang <lichuang@staff.sina.com>
2023-06-16 20:07:21 +0800

    2023-06-16

---
+++ src/main/java/site/muzhi/leetcode/offerII/$79_subsets.java
@@ -0,0 +1,29 @@
+package site.muzhi.leetcode.offerII;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author lichuang
+ * @date 2023/06/16
+ * 79.所有子集
+ */
+public class $79_subsets {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        travel(ans, new ArrayDeque<>(), nums, 0);
+        return ans;
+    }
+
+    private void travel(List<List<Integer>> ans, Deque<Integer> curr, int[] nums, int idx) {
+        ans.add(new ArrayList<>(curr));
+        for (int i = idx; i < nums.length; i++) {
+            curr.addLast(nums[i]);
+            travel(ans, curr, nums, i + 1);
+            curr.removeLast();
+        }
+    }
+}
---
+++ src/main/java/site/muzhi/leetcode/offerII/$81_combinationSum.java
@@ -0,0 +1,37 @@
+package site.muzhi.leetcode.offerII;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author lichuang
+ * @date 2023/06/16
+ * 81.允许重复选择元素的组合
+ */
+public class $81_combinationSum {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        travel(ans, new ArrayDeque<>(), 0, candidates, 0, target);
+        return ans;
+    }
+
+    private void travel(List<List<Integer>> ans, Deque<Integer> curr, int currSum, int[] candidates, int idx, int target) {
+        if (currSum > target) {
+            return;
+        }
+        if (currSum == target) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = idx; i < candidates.length; i++) {
+            curr.add(candidates[i]);
+            travel(ans, curr, currSum + candidates[i], candidates, i, target);
+            curr.removeLast();
+        }
+    }
+
+}
