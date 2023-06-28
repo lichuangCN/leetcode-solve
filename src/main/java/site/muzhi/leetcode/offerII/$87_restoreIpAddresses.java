8dcca543c64fae70a98c471dc3b0005a63cb4cad
lichuang <lichuang@staff.sina.com>
2023-06-28 19:48:20 +0800

    2023-06-28

---
+++ src/main/java/site/muzhi/leetcode/offerII/$87_restoreIpAddresses.java
@@ -0,0 +1,66 @@
+package site.muzhi.leetcode.offerII;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author lichuang
+ * @date 2023/06/28
+ * 87.复原IP
+ */
+public class $87_restoreIpAddresses {
+
+    public static void main(String[] args) {
+        $87_restoreIpAddresses demo = new $87_restoreIpAddresses();
+        List<String> ans = demo.restoreIpAddresses("25525511135");
+        ans.forEach( e-> {
+            System.out.println(e);
+        });
+    }
+
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+
+        travel(ans, new ArrayDeque<>(), s.toCharArray(), 0);
+        return ans;
+    }
+
+    private void travel(List<String> ans, Deque<String> curr, char[] arr, int idx) {
+        // 满足条件
+        if (curr.size() == 4 && idx == arr.length) {
+            ans.add(String.join(".", curr));
+            return;
+        }
+
+        // 后续已经不满足条件
+        if (curr.size() == 4 && idx != arr.length) {
+            return;
+        }
+
+        // 向后寻找三位字符
+        for (int i = idx; i < idx + 3; i++) {
+            // 超过字符串长度 终止
+            if (i >= arr.length) {
+                break;
+            }
+            // 校验 [i,i]/[i,i+1]/[i,i+2]
+            String c = new String(arr, idx, i - idx + 1);
+            if (check(c)) {
+                curr.addLast(c);
+                travel(ans, curr, arr, i + 1);
+                curr.removeLast();
+            }
+        }
+    }
+
+    private boolean check(String s) {
+        // 010 ...
+        if (s.length() > 1 && s.charAt(0) == '0') {
+            return false;
+        }
+        Integer num = Integer.valueOf(s);
+        return num >= 0 && num <= 255;
+    }
+}
